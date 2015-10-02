348e70b38c986620c183907944d020a29014bdf4
andrey <ag82pishi@yandex.ru>
2015-10-02 12:03:56 +0300

    rspec: adds global macros

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe ItemsController do
+
+  it_renders_404_page_when_item_is_not_found :show, :edit, :update, :destroy
   
   describe "show action" do
     it "renders show template if an item is found" do
@@ -9,10 +11,10 @@ describe ItemsController do
       expect(response).to render_template(:show)
     end
 
-    it "renders 404 page if an item is not found" do
-      get :show, id: 0
-      expect(response.status).to eq(404)
-    end
+    # it "renders 404 page if an item is not found" do
+    #   get :show, id: 0
+    #   expect(response.status).to eq(404)
+    # end
   end
 
   describe "create action" do
@@ -29,6 +31,19 @@ describe ItemsController do
     # it "renders 403 page if user is not an admin" do
     #   post :create, item: { name: "Item 1", price: '10' }
     #   expect(response.status).to eq(403)
+    # end
+  end
+
+  describe "destroy action" do
+    it "redirects to index action when an item is destroyed successfully" do
+      item = create(:item)
+      delete :destroy, id: item.id
+      expect(response).to redirect_to items_path
+    end
+
+    # it "renders 404 page if item not found" do
+    #   delete :destroy, id: 0
+    #   expect(response.status).to eq(404)
     # end
   end
 end
